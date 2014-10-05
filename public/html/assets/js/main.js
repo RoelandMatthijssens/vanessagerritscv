97b7442f67753001e1acc5714621a9ecb213af75
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2014-10-05 19:51:18 +0200

    added new deisgn

---
+++ public/html/assets/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+	$(".cv_link").on("click", function(){
+		openCv($(this));
+	});
+});
+
+function openCv(object){
+	var toggle = object.hasClass("active");
+	$(".cv_link").removeClass("active");
+	if(!toggle){
+		object.addClass("active");
+		openSideContent();
+	}else{
+		closeSideContent();
+	}
+	var container = $(".side_content");
+	var cvNames = object.data("cv").split(",");
+	container.html("");
+	for(var i in cvNames){
+		var cvName = cvNames[i];
+		console.log(cvName);
+		var cvPath = "assets/img/"+cvName+".jpg";
+		var img = $("<img/>");
+		img.attr("src", cvPath);
+		$(".side_content").append(img);
+	}
+}
+
+function openSideContent(){
+	var sideContent = $(".side_content");
+	var mainContent = $(".main_content");
+	if(sideContent.hasClass("hidden")){
+		mainContent.animate({
+			marginLeft: "-=20%"
+		}, 500, function(){
+			sideContent.removeClass("hidden");
+		});
+		//mainContent.css("margin-left", "10%");
+	}
+}
+
+function closeSideContent(){
+	var sideContent = $(".side_content");
+	var mainContent = $(".main_content");
+	sideContent.addClass("hidden");
+		mainContent.animate({
+			marginLeft: "+=20%"
+		}, 500);
+}
